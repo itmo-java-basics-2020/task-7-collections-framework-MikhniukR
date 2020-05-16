3ca92d1e8c140d417cb5c3577ea27aef548667e2
Roma <r.mikhniuk@gmail.com>
2020-05-16 16:07:38 +0300

    Solve lab-7

--- src/main/java/ru/ifmo/collections/FirstUnique.java
+++ src/main/java/ru/ifmo/collections/FirstUnique.java
@@ -1,19 +1,49 @@
 package ru.ifmo.collections;
 
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
 /**
  * Design a class which contains integers and returns first unique integer (in order of addition).
  * FirstUniqueTest can be used as an example.
  */
 public class FirstUnique {
+
+    private Set<Integer> unique;
+    private Set<Integer> noUnique;
+    private Queue<Integer> order;
+
     public FirstUnique(int[] numbers) {
-        // TODO implement
+        unique = new HashSet<>();
+        noUnique = new HashSet<>();
+        order = new ArrayDeque<>();
+
+        for (int number : numbers) {
+            add(number);
+        }
     }
 
     public int showFirstUnique() {
-        throw new UnsupportedOperationException(); // TODO implement
+        while (!order.isEmpty()) {
+            if (noUnique.contains(order.element())) {
+                order.poll();
+            } else {
+                return order.element();
+            }
+        }
+
+        return -1;
     }
 
     public void add(int value) {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (!unique.contains(value)) {
+            unique.add(value);
+            order.add(value);
+        } else {
+            unique.remove(value);
+            noUnique.add(value);
+        }
     }
 }
